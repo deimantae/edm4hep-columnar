9d349fa24917891a33447d0537fbbd968057a05e
Deimante Jukneviciute <deimante.jukneviciute@ff.stud.vu.lt>
2026-08-26 08:48:31 +0200

    Install Coffea dependencies only when needed

--- Coffea-FCCAnalyses/setup.sh
+++ Coffea-FCCAnalyses/setup.sh
@@ -14,17 +14,21 @@ SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 unset PYTHONPATH
 unset PYTHONHOME
 
-# Create the virtual environment
+# Create the virtual environment and install dependencies if needed
 if [ ! -d "${SCRIPT_DIR}/.venv" ]; then
     echo "----> Creating Python virtual environment..."
     python3 -m venv "${SCRIPT_DIR}/.venv"
+
+    echo "----> Activating Python virtual environment..."
+    source "${SCRIPT_DIR}/.venv/bin/activate"
+
+    # Install everything listed in requirements.txt
+    echo "----> Installing Python dependencies..."
+    python -m pip install -q -r "${SCRIPT_DIR}/requirements.txt"
+
+else
+    echo "----> Activating Python virtual environment..."
+    source "${SCRIPT_DIR}/.venv/bin/activate"
 fi
-
-echo "----> Activating Python virtual environment..."
-source "${SCRIPT_DIR}/.venv/bin/activate"
-
-# Install everything listed in requirements.txt
-echo "----> Installing Python dependencies..."
-python -m pip install -q -r "${SCRIPT_DIR}/requirements.txt"
 
 echo "----> Coffea environment ready"
